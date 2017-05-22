1c8dd6eb3d1cf077287c96418591d40f962d4529
Jamey Hanson <jamesphanson@yahoo.com>
2017-05-22 11:33:04 -0400

    move c

--- test_loop.sh
+++ test_loop.sh
@@ -20,14 +20,14 @@ do
     # middle loop for run number
     for run_num in {1..3}
     do
+        echo "clean output directory"
+        hdfs dfs -rm -r -skipTrash /results/*
+    
         # inner loop for pig script
         for script in $scripts
         do
             echo $framework $script
             /usr/local/pig-0.16.0/bin/pig -x $framework $script
         done
-        
-        echo "delete output"
-        hdfs dfs -rm -r -skipTrash /results/*
     done
 done
