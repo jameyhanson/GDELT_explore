817a11094001ec40d27ee354d988a0579b6d13ef
Jamey Hanson <jamesphanson@yahoo.com>
2017-05-26 12:34:03 -0400

    removed comments to make it simpler to cut & paste

--- create_subset_directory.sh
+++ create_subset_directory.sh
@@ -32,15 +32,17 @@ hdfs dfs -cp /data/gdelt_v2/events/20170402.export.csv /data/subset_gdelt_v2/eve
 hdfs dfs -cp /data/gdelt_v2/events/20170502.export.csv /data/subset_gdelt_v2/events/
 
 # swap directories for development / testing
-# hdfs dfs -mv /data/gdelt_v1 /data/full_gdelt_v1
-# hdfs dfs -mv /data/gdelt_v2 /data/full_gdelt_v2
+# use if /data/subset_gdelt_v? exists
+hdfs dfs -mv /data/gdelt_v1 /data/full_gdelt_v1
+hdfs dfs -mv /data/gdelt_v2 /data/full_gdelt_v2
 
-# hdfs dfs -mv /data/subset_gdelt_v1 /data/gdelt_v1
-# hdfs dfs -mv /data/subset_gdelt_v2 /data/gdelt_v2
+hdfs dfs -mv /data/subset_gdelt_v1 /data/gdelt_v1
+hdfs dfs -mv /data/subset_gdelt_v2 /data/gdelt_v2
 
 # swap directories for full load
-# hdfs dfs -mv /data/gdelt_v1 /data/subset_gdelt_v1
-# hdfs dfs -mv /data/gdelt_v2 /data/subset_gdelt_v2
+# use if /data/full_gdelt_v? exists
+hdfs dfs -mv /data/gdelt_v1 /data/subset_gdelt_v1
+hdfs dfs -mv /data/gdelt_v2 /data/subset_gdelt_v2
 
-# hdfs dfs -mv /data/full_gdelt_v1 /data/gdelt_v1
-# hdfs dfs -mv /data/full_gdelt_v2 /data/gdelt_v2
+hdfs dfs -mv /data/full_gdelt_v1 /data/gdelt_v1
+hdfs dfs -mv /data/full_gdelt_v2 /data/gdelt_v2
