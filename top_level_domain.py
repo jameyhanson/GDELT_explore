3719dfe3d7f4d0abbb70cd538dab72d875a36244
Jamey Hanson <jamesphanson@yahoo.com>
2017-06-08 09:42:07 -0400

    Create top_level_domain.py

--- top_level_domain.py
+++ top_level_domain.py
@@ -1,13 +1,6 @@
-Register 'top_level_domain.py' using jython as example_udf;
+# top_level_domain.py
 
-------------------------------------------
-top_level_domain.py
-------------------------------------------
  #tld- return the top level domain
  @outputSchema("tld:chararray")
  def tld(host):
      return host[host.rfind('.')+1:]
-
-
- ------------------------------------------
- 
