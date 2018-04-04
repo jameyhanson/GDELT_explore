4b1fedd30357622eee99991f97ddf0d3a80f26d6
Jamey Hanson <jameyphanson@gmail.com>
2018-04-04 13:11:33 -0400

    copied from run_scripts_in_loop.sh

---
+++ run_scripts_in_encr_loop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# see http://ryanstutorials.net/bash-scripting-tutorial/bash-loops.php
+
+# loop through programs in mapreduce and tez
+
+# scripts to test
+read -d '' scripts << EOF
+count_GDELT.pig
+AvgTone_USA_ntiles_by_day.pig
+AvgTone_USA_ntiles_by_month.pig
+AvgTone_USA_ntiles_by_year.pig
+AvgTone_USA_ntiles_by_congress.pig
+EOF
+
+# frameworks='tez mapreduce' # uncomment when using tez
+# outer loop for frameworks
+for framework in $frameworks
+do
+    echo "####### $framework tests ######"
+    
+    # middle loop for run number
+    for run_num in {1..3}
+    do
+        echo "clean output directory"
+        hdfs dfs -rm -r -skipTrash /results/*
+    
+        # inner loop for pig script
+        for script in $scripts
+        do
+            echo $framework $script
+            # NOTE:  applicaiton tags do not work with Tez
+            export HADOOP_OPTS=-Dmapreduce.job.tags=trial_number:_$run_num
+            /usr/local/pig-0.16.0/bin/pig -x $framework $script
+        done
+    done
+done
